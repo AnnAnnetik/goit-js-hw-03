23537a0e20d21539214cc1eb82badb6eacf64415
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2023-11-18 20:56:44 +0200

    дз 3

---
+++ js/test-2.js
@@ -0,0 +1,17 @@
+function makeArray (firstArray, secondArray, maxLength){
+    let makeArray2=firstArray.concat(secondArray);
+    if(makeArray2.length>maxLength){
+        return makeArray2.slice(0, maxLength);
+    }
+    else{
+        return makeArray2;
+    }
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
