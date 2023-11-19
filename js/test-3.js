40573517e0822a36907c94e72da51cc7c990701c
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2023-11-19 18:40:14 +0200

    3

--- js/test-3.js
+++ js/test-3.js
@@ -1,7 +1,8 @@
 function filterArray(numbers, value){
     let newArray =[];
     for(let i = 0; i < numbers.length; i += 1){
-        if (numbers[i]>value){
+        if (numbers[i]>value)
+        {
             newArray.push(numbers[i]);
         }
     } return newArray;
